2ab4440f21b923abb9bec08547f956bcc46d6519
wow9144 <suhoo.kim2@gmail.com>
2020-08-16 10:28:30 +0900

    day-6 homework

--- day-6/src/log.js
+++ day-6/src/log.js
@@ -1,14 +1,29 @@
 const fs = require("fs")
 const dateFns = require("date-fns")
 
+let fileName = ""
 function log(message) {
-    const date = dateFns.format(new Date(), "yyyy.MM.dd HH:mm:ss")
+    const date = new Date();
+    const logDate = dateFns.format(date, "yyyy.MM.dd HH:mm:ss")
+    fileName = `log_${dateFns.format(date, "yyy_MM_dd_HH")}.txt`
 
-    fs.appendFile("log.txt", `${date} :: ${message}\n`, "utf-8", (err) => {
+    fs.appendFile(fileName, `${logDate} :: ${message}\n`, "utf-8", (err) => {
       if (err) {
           console.log(err)
       }
     })
 }
 
-module.exports = log
+const readLog = (cb) => {
+    fs.readFile(fileName, "utf-8", (err, data) => {
+        if (err) {
+            return res.end("err")
+        }
+        return cb(data)
+    })
+}
+
+module.exports = {
+    log: log,
+    readLog: readLog
+}
--- day-6/src/server.js
+++ day-6/src/server.js
@@ -1,10 +1,13 @@
 const http = require("http");
-const log = require("./log");
+const logger = require("./log");
+
 const server = http.createServer((req, res) => {
-    log(req.headers["user-agent"])
+    logger.log(req.headers["user-agent"])
+
     switch (req.url) {
         case "/about": return res.end("about suho")
         case "/mypage": return res.end("mypage suho")
+        case "/log": return logger.readLog((data) => res.end(data));
         default: return res.end("404 suho")
     }
 });
