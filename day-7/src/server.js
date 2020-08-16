63e982c9407dd9b0517227110d7633627d1c387d
wow9144 <suhoo.kim2@gmail.com>
2020-08-16 11:45:04 +0900

    day-7 user component

--- day-7/src/server.js
+++ day-7/src/server.js
@@ -1,10 +1,25 @@
 const http = require("http")
 const readFile = require("./readFile")
 
+const users = [{
+    name: "suho",
+    age: 36
+}, {
+    name: "gildong",
+    age: 20
+}]
+function userComponent(users) {
+    const userLis = users.map((user) => {
+        return `<li>${user.name} : ${user.age}</li>`
+    }).join("")
+    return `<ul>${userLis}</ul>`
+}
 http
   .createServer((req, res) => {
     if (req.url === "/") {
-      return readFile("index", (data) => res.end(data))
+        return readFile("index", (data) => res.end(data))
+    } else if(req.url === "/users") {
+        return res.end(`<html><body>${userComponent(users)}</body></html>`)
     } else if (req.url === "/users") {
       return readFile("users", (data) => res.end(data))
     } else {
