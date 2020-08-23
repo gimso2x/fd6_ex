db8806d83de723ca685a7af4cf8fb9b4c81c30f4
wow9144 <suhoo.kim2@gmail.com>
2020-08-23 13:08:28 +0900

    react

--- day-8-react/src/index.js
+++ day-8-react/src/index.js
@@ -1,14 +1,19 @@
 import ReactDOM from "react-dom"
 import React from "react"
+import App from "./App";
 
 const h1 = React.createElement(
   "h1",
   {
     style: {color: "blue"}
   },
-  "Hello World!!!!"
+  "Hello"
 )
-
-ReactDOM.render(h1,
+const span = React.createElement(
+  "span",
+  {style: {color: "red"}},
+  h1, `World!`
+)
+ReactDOM.render(<App />,
   document.querySelector("#root")
 )
